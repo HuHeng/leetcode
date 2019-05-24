1b5dd06a4abc864bf7a73d4f09cdbfc17a96baff
huheng <huheng@mudu.tv>
2019-05-24 23:31:05 +0800

    42

--- 42_trapping_rain_water.cc
+++ 42_trapping_rain_water.cc
@@ -5,14 +5,43 @@
 
 using namespace std;
 
+
+//use two pointer
+
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while(left < right) {
+            if(height[left] <= height[right]) {
+                if(height[left] < left_max){
+                    ans += left_max - height[left];
+
+                }else {
+                    left_max = height[left];
+                }
+                left++;
+
+            } else {
+                if(height[right] < right_max) {
+                    ans += right_max - height[right];
+                } else {
+                    right_max = height[right];
+                }
+
+                right--;
+            }
+        }
+        return ans;
     
     }
 };
 
 int main()
 {
-
+    Solution s;
+    std::vector<int> height{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    std::cout << s.trap(height) << std::endl;
 }
